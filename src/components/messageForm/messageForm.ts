f5a661055d18d0f3f727c30303f8c8c1e6892dba
Ворожцов Артем Ильич <a_vorozhtsov@step.ru>
2024-03-06 18:58:39 +0300

    fixing code review remarks 2.1

--- src/components/messageForm/messageForm.ts
+++ src/components/messageForm/messageForm.ts
@@ -7,25 +7,6 @@ class MessageForm extends Block {
     constructor(props: Props) {
         super("div", props, MessageFormTemplate);
         
-        document.addEventListener('DOMContentLoaded', (_event) => {
-            const formElement = this.element?.querySelector("form");
-            const messageInput = this.element?.querySelector("input");
-            formElement?.addEventListener('submit', (event: Event) => {
-                event.preventDefault(); 
-                let isValid = false;
-                if (messageInput){
-                        
-                    isValid = validateInput("message" as ValidationRule, messageInput.value);
-                }
-                
-                
-                if (isValid) {
-                    console.log("Отправлено сообщение " + messageInput?.value)
-                    }
-                
-                
-        });
-        });
         
     }
 
