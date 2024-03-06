d97b090f44b2e72ea8385095b96a2bd9b55c702e
Ворожцов Артем Ильич <a_vorozhtsov@step.ru>
2024-03-06 19:00:00 +0300

    fixing test errors 17

--- src/components/messageForm/messageForm.ts
+++ src/components/messageForm/messageForm.ts
@@ -1,6 +1,5 @@
 import Block, {Props}  from "../block/block";
 import MessageFormTemplate from "./messageForm.hbs?raw";
-import validateInput, { ValidationRule } from "../../utils/validation";
 
 
 class MessageForm extends Block {
